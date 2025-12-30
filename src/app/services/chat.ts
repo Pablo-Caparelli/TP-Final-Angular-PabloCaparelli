5bdf912d7bc8e0768c29fd51733f437a8fd314d3
pablo-caparelli <pablocaparelli@gmail.com>
2025-12-30 16:41:58 -0300

    funcionalidad de localstorage para que los contactos queden cargados

--- src/app/services/chat.ts
+++ src/app/services/chat.ts
@@ -7,9 +7,31 @@ import { Message } from '../../interfaces/message';
 })
 export class ChatService {
   /* Mook de chats guardado */
-  private _chats: WritableSignal<Chat[]> = signal(this.createMook());
+  /*private _chats: WritableSignal<Chat[]> = signal(this.createMook());
   public readonly chats: Signal<Chat[]> = this._chats;
+  constructor() {}*/
+  private readonly STORAGE_KEY = 'chats-data';
+
+  private _chats: WritableSignal<Chat[]> = signal(this.loadChats());
+  public readonly chats: Signal<Chat[]> = this._chats;
+
   constructor() {}
+
+  private loadChats(): Chat[] {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    if (data) {
+      try {
+        return JSON.parse(data);
+      } catch {
+        return this.createMook();
+      }
+    }
+    return this.createMook();
+  }
+
+  private saveChats(chats: Chat[]) {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(chats));
+  }
 
   /* CARGA DATOS mook (de prueba) */
   private createMook(): Chat[] {
@@ -60,14 +82,25 @@ export class ChatService {
       messages: [],
     };
     /* Actualizo la señal */
-    this._chats.update((chats_actuales) => {
+    /*this._chats.update((chats_actuales) => {
       return [...chats_actuales, new_chat];
+    });
+    return new_chat;*/
+    this._chats.update((chats) => {
+      const updated = [...chats, new_chat];
+      this.saveChats(updated);
+      return updated;
     });
     return new_chat;
   }
 
   deleteChat(contactId: string) {
-    this._chats.update((contacts) => contacts.filter((contact) => contact.id !== contactId));
+    //this._chats.update((contacts) => contacts.filter((contact) => contact.id !== contactId));
+    this._chats.update((chats) => {
+      const updated = chats.filter((c) => c.id !== contactId);
+      this.saveChats(updated);
+      return updated;
+    });
   }
 
   sendMessage(chat_id: string, text: string, fromMe = true): Message | undefined {
@@ -78,10 +111,10 @@ export class ChatService {
       date: new Date().toISOString(),
     };
 
-    this._chats.update((chats_actuales) => {
+    /*this._chats.update((chats_actuales) => {
       return chats_actuales.map((chat) => {
         /* Si no son el chat que quiero actualizar dejo el mensaje asi como esta */
-        if (chat.id !== chat_id) {
+    /*if (chat.id !== chat_id) {
           return chat;
         }
         const updated_messages = [...chat.messages, new_message];
@@ -91,6 +124,25 @@ export class ChatService {
           lastMessage: text,
         };
       });
+    });
+    return new_message;
+  }
+}*/
+    this._chats.update((chats) => {
+      const updated = chats.map((chat) => {
+        if (chat.id !== chat_id) return chat;
+
+        const updated_messages = [...chat.messages, new_message];
+
+        return {
+          ...chat,
+          messages: updated_messages,
+          lastMessage: text,
+        };
+      });
+
+      this.saveChats(updated);
+      return updated;
     });
     return new_message;
   }
